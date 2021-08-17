89f5f22505d28fef3938399e49ddf9670933be2e
Will Leyton <wleylop22@gmail.com>
2021-08-17 07:48:16 -0500

    Add get middle

---
+++ src/Kata/GetMiddle/Solution.hs
@@ -0,0 +1,34 @@
+module Kata.GetMiddle.Solution where
+
+-- Problem: https://www.codewars.com/kata/56747fd5cb988479af000028
+
+-- | getMiddle
+-- >>> getMiddle "test"
+-- "es"
+-- >>> getMiddle "testing"
+-- "t"
+-- >>> getMiddle "A"
+-- "A"
+getMiddle :: String -> String
+getMiddle "" = ""
+getMiddle [a] = [a]
+getMiddle x@[_, _] = x
+getMiddle (_ : xs) = getMiddle $ init xs
+
+{-
+getMiddle :: String -> String
+getMiddle [] = []
+getMiddle word =
+  let size = length word
+      middle = size `div` 2
+   in reduce word size middle
+  where
+    reduce :: String -> Int -> Int -> String
+    reduce word' size' middle'
+      | even size' =
+        let (h, t) = splitAt middle' word'
+         in [last h, head t]
+      | otherwise =
+        let (h, _) = splitAt (middle' + 1) word'
+         in [last h]
+-}
