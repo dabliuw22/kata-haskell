671ace33046436a61ae175e1f6c623cf83bdd5c2
Will Leyton <wleylop22@gmail.com>
2021-08-19 08:56:34 -0500

    Add Lib

---
+++ src/Lib.hs
@@ -0,0 +1,7 @@
+module Lib (module Kata) where
+
+import Kata.DigitalRoot.Solution as Kata
+import Kata.DoubleCola.Solution as Kata
+import Kata.GetMiddle.Solution as Kata
+import Kata.Likes.Solution as Kata
+import Kata.UniqueInOrder.Solution as Kata
